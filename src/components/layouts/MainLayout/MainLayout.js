b0c6fb2c7c72f98fc93b8f3137fdfaa805ba04ce
nazariipavliuk <n.pavliuk.webdev@gmail.com>
2022-12-10 11:29:25 +0200

    Added helmet for app

--- src/components/layouts/MainLayout/MainLayout.js
+++ src/components/layouts/MainLayout/MainLayout.js
@@ -1,8 +1,14 @@
 import styles from '@components/layouts/MainLayout/MainLayout.module.scss'
+import { Helmet } from 'react-helmet'
+import { APP_NAME } from '@utils/constsants/general'
 
 export const MainLayout = ({children}) => {
   return (
-    <div  className={styles.page}>
+    <div className={styles.page}>
+      <Helmet>
+        <meta charSet="utf-8"/>
+        <title>{APP_NAME}</title>
+      </Helmet>
       <main>{children}</main>
     </div>
   )
---
+++ src/utils/constsants/general.js
@@ -0,0 +1,3 @@
+const APP_NAME = 'Branch Name Generator'
+
+export { APP_NAME }
